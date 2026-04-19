03cb8b84cd643a6341cf649f67b3f33d281523ff
Tal Samucha <tal.s@sealsecurity.io>
2026-04-19 12:53:50 +0300

    revert

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,8 +18,7 @@ module.exports = function(grunt) {
             'es5',
             'es2015.promise',
             'dom'
-          ],
-          types: []
+          ]
         },
         src: ['typings/index.d.ts', 'test/typescript/*.ts']
       }
@@ -102,7 +101,7 @@ module.exports = function(grunt) {
     grunt.file.write('bower.json', JSON.stringify(bower, null, 2));
   });
 
-  grunt.registerTask('test', 'Run the jasmine and mocha tests', ['eslint', 'mochaTest', 'karma:single', 'ts']);
+  grunt.registerTask('test', 'Run the jasmine and mocha tests', ['eslint', 'mochaTest', 'karma:single']);
   grunt.registerTask('build', 'Run webpack and bundle the source', ['clean', 'webpack']);
   grunt.registerTask('version', 'Sync version info for a release', ['usebanner', 'package2bower']);
 };
