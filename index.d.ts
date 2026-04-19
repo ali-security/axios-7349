a31df12e1e3b5ec2d915ff767d5354f6bc42c62d
Tal Samucha <tal.s@sealsecurity.io>
2026-04-19 14:47:37 +0300

    revert

--- index.d.ts
+++ index.d.ts
@@ -51,7 +51,6 @@ export interface AxiosRequestConfig<T = any> {
   url?: string;
   method?: Method;
   baseURL?: string;
-  allowAbsoluteUrls?: boolean;
   transformRequest?: AxiosTransformer | AxiosTransformer[];
   transformResponse?: AxiosTransformer | AxiosTransformer[];
   headers?: Record<string, string>;
